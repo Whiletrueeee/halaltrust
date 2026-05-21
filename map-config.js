ad4c2f4cbf5f12c78e3daebcb27412a4fbc53643
Whiletrueeee <lbj258995813@gmail.com>
2026-05-21 09:58:44 +0800

    Delete map-config.js

--- map-config.js
+++
@@ -1,6 +0,0 @@
-window.HALAL_MAP_CONFIG = {
-  // 填入高德 Web 端 JS API Key 后，会自动启用高德地图和高德地点查询。
-  amapKey: "515ad61d9714b1cd1b55e4619f7410da",
-  // 如果你的高德应用开启了安全密钥校验，在这里填安全密钥。
-  amapSecurityCode: "10db6769963a315c561592ccb2bd6258"
-};
